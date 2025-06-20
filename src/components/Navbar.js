99ed7d3fdd19eca467467062605ba3f0668c60bf
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-20 17:34:52 +0700

    still tweaking the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,31 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <section className="absolute w-full h-full ">
       <nav className="flex justify-between items-center gap-4 py-4 md:px-25 font-semibold shadow-sm">
         <span className="font-bold text-lg">Helart Logo</span>
-        <div className="flex justify-center gap-20">
-          <span>Dashboard</span>
-          <span>Support</span>
+        <div className="flex justify-center gap-10">
+          <Link 
+            href={'#'}
+            className="hover:ring-foreground hover:ring-2 transition-all duration-150 px-5 py-2 rounded-md"
+          >
+            Dashboard
+          </Link>
+          <Link 
+            href={'#'}
+            className="hover:ring-foreground hover:ring-2 transition-all duration-150 px-5 py-2 rounded-md"
+          >
+            Support
+          </Link>
         </div>
-        <span 
+        <Link
+          href={'#'}
           className="px-5 py-2 rounded-md bg-foreground text-background text-lg font-normal
           hover:bg-dark-foreground"
         >
           Login
-        </span>
+        </Link>
       </nav>
     </section>
   );
