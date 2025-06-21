308c8600a6b868b946ec61633d29b6b636c5ba86
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-21 17:36:41 +0700

    Adjusting Button component contents

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
-export default function Button({ child, to = '#', className }) {
+export default function Button({ child, to = '#', className, optIcon, iconDiv }) {
 
   return(
-    <Link href={to} className={`z-1 border-2 text-lg min-w-38 min-h-12 px-3 py-2 flex items-center justify-center rounded-xl cursor-pointer transition-all duration-150 ${className}`}>
+    <Link href={to} className={`z-1 border-2 text-lg min-w-38 min-h-12 px-5 py-2 flex items-center justify-center gap-3 rounded-xl cursor-pointer transition-all duration-150 ${className}`}>
         {child}
+        <div className={iconDiv}>{optIcon}</div>
     </Link>
   );
 }
