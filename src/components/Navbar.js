c65b0b502a80899139ac727a97c8c757eaf204a0
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-21 17:37:15 +0700

    Adjusting nabbar gap and padding x

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <section className="absolute w-full h-full ">
-      <nav className={`flex justify-between items-center gap-4 py-4 md:px-25 font-semibold shadow-sm shadow-foreground/20`}>
+      <nav className={`flex justify-between items-center gap-4 py-4 md:px-40 font-semibold shadow-sm shadow-foreground/20`}>
         <span className="font-bold text-lg">Helart Logo</span>
-        <div className="flex justify-center gap-10">
+        <div className="flex justify-center gap-8">
           <Link 
             href={'#'}
             className="hover:ring-foreground hover:ring-2 transition-all duration-150 px-5 py-2 rounded-md"
